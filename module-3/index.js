14ae9936efc932cc0c2f48d7217e24d8c981f50d
codewithshamim <shamimislamonline@gmail.com>
2022-08-31 22:38:52 +0600

    routes config

--- module-3/index.js
+++ module-3/index.js
@@ -2,10 +2,19 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 5000;
+const toolsRoutes = require("./routes/v1/tools.route");
+const { database } = require("./utils/database");
 
 app.use(cors());
 app.use(express.json());
 
+database();
+
+app.use("/api/v1/tools", toolsRoutes)
+
+app.all("*", (req, res) => {
+    res.send("Not found");
+});
 app.listen(port, () => {
-    console.log("server is running....")
+    console.log("server is running....");
 });
---
+++ module-3/routes/v1/tools.route.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+
+router.route("/")
+    /**
+     * @api {get} /tools All tools
+     * @apiDescription Get all the tools
+     * @apiPermission admin
+     *
+     * @apiHeader {String} Authorization   User's access token
+     *
+     * @apiParam  {Number{1-}}         [page=1]     List page
+     * @apiParam  {Number{1-100}}      [limit=10]  Users per page
+     *
+     * @apiSuccess {Object[]} all the tools.
+     *
+     * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
+     * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
+     */
+    .get((req, res) => {
+        res.send("tools get page")
+    })
+    /**
+     * @api {get} /save All tools
+     * @apiDescription Get all the tools
+     * @apiPermission admin
+     *
+     * @apiHeader {String} Authorization   User's access token
+     *
+     * @apiParam  {Number{1-}}         [page=1]     List page
+     * @apiParam  {Number{1-100}}      [limit=10]  Users per page
+     *
+     * @apiSuccess {Object[]} all the tools.
+     *
+     * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
+     * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
+     */
+    .post((req, res) => {
+        res.send("tools post page")
+    })
+
+module.exports = router;
