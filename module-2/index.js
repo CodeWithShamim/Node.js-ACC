02ad188eea8b72956bb9d0ddca273c436fd8d428
codewithshamim <shamimislamonline@gmail.com>
2022-11-14 11:36:07 +0600

    updated

--- module-2/index.js
+++ module-2/index.js
@@ -1,11 +1,15 @@
 const http = require('http');
-const port = 8000;
+const url = require('url');
+const port = 5000;
+const backend_url = "http://localhost:5000/?new=1&old=2"
 
 const server = http.createServer((req, res) => {
     if (req.url === "/") {
-        res.write("Server is running.....");
+        res.write('<h1>Hello</h1>');
         res.end();
     }
+    // const parsedUrl = url.parse(backend_url, true)
+    // console.log(parsedUrl.query);
 })
 
 server.listen(port);
