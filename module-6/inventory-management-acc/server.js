17bfd66e62718f709e191e0360c2f1e557150900
codewithshamim <shamimislamonline@gmail.com>
2022-11-17 11:52:34 +0600

    schema added

--- module-6/inventory-management-acc/server.js
+++ module-6/inventory-management-acc/server.js
@@ -15,6 +15,76 @@ mongoose.connect(process.env.DB_LOCAl)
   .then(() => console.log("DB connect success......".red.bold))
   .catch((error) => console.log("Error", error.message))
 
+// schema design 
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide a name for this product."],
+    unique: [true, "product name must be unique."],
+    minLength: [3, "Product name must be at least 3 characters."],
+    maxLength: [100, "Product name is too large."],
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: [0, "Name can't be negative."]
+  },
+  unit: {
+    type: String,
+    required: true,
+    enum: {
+      values: ["kg", "litre", "pcs"],
+      message: "Unit value can't be {VALUE}. must be kg/litre/pcs"
+    }
+
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: [0, "Quantity can't be negative"],
+    validate: {
+      validator: (value) => {
+        const isInteger = Number.isInteger(value);
+        isInteger ? true : false
+      }
+    },
+    message: "Quantity must be an integer."
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: {
+      values: ["in-stock", "out-of-stock", "discontinued"],
+      message: "status can't be {VALUE}"
+    }
+  },
+  // createdAt: {
+  //   type: Date,
+  //   default: Date.now
+  // },
+  // updatedAt: {
+  //   type: Date,
+  //   default: Date.now
+  // },
+  supplier: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Supplier"
+  },
+  categories: [{
+    _id: mongoose.Schema.Types.ObjectId,
+    name: {
+      type: String,
+      required: true
+    }
+  }]
+
+}, { timestamps: true })
+
 // route 
 app.get("/", (req, res) => {
   res.send("ok")
