7fb0299d85585f6122f8a6bb7ebbfd4b9adffc27
codewithshamim <shamimislamonline@gmail.com>
2022-11-17 18:13:36 +0600

    query data using mongoose

--- module-6/inventory-management-acc/server.js
+++ module-6/inventory-management-acc/server.js
@@ -89,7 +89,7 @@ const productSchema = mongoose.Schema({
 
 // mongoose middlewares => pre - post
 productSchema.pre("save", function (next) {
-  console.log("Before saving data.");
+  // console.log("Before saving data.");
   // this=>
   if (this.quantity === 0) {
     this.status = "out-of-stock";
@@ -97,7 +97,7 @@ productSchema.pre("save", function (next) {
   next()
 })
 productSchema.post("save", function (doc, next) {
-  console.log("after saving data.");
+  // console.log("after saving data.");
   next()
 })
 
@@ -124,7 +124,7 @@ app.post("/api/v1/product", async (req, res) => {
     //   product.status = "out-of-stock";
     // }
     product.logger()
-    
+
     const result = await product.save();
     // const result = await Product.create(data);
     res.status(200).json({
@@ -136,6 +136,40 @@ app.post("/api/v1/product", async (req, res) => {
     res.status(400).json({
       success: false,
       message: "data is not inserted.",
+      error: error.message
+    })
+  }
+})
+
+// get products 
+app.get("/api/v1/product", async (req, res) => {
+  try {
+    // const products = await Product.find({_id:"63761e21af65ea0c70ba3eb8"});
+    // const products = await Product.find({status:"in-stock"});
+    // const products = await Product.find({ $or: [{ _id: "63761e21af65ea0c70ba3eb8" }, { name: "dkjsdl" }] });
+    // const products = await Product.find({ status: { $ne: "out-of-stock" } });
+    // const products = await Product.find({ name: { $in: ["mobile", "s5t24"] } });
+
+    // const products = await Product.find({}, "name quantity");
+    // const products = await Product.find({}, "-createdAt -updatedAt -_id -__v");
+    // const products = await Product.find().select({ name: 1 });
+
+    // const products = await Product.find().sort({ price: 1 });
+    // const products = await Product.findById("63761e21af65ea0c70ba3eb8");
+
+    const products = await Product
+      .where("status").equals("out-of-stock")
+      .where("price").gt(1).lt(100)
+      .limit(2).sort({ quantity: 1 })
+
+    res.status(200).json({
+      success: true,
+      data: products
+    })
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: "can't get the data.",
       error: error.message
     })
   }
