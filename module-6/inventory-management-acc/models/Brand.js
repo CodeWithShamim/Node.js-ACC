76b1b3ed860237394f9a938c42db5dd785ad1148
codewithshamim <shamimislamonline@gmail.com>
2022-11-21 20:56:11 +0600

    Brand model added

---
+++ module-6/inventory-management-acc/models/Brand.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const { ObjectId } = mongoose.Schema.Types;
+
+const brandSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        maxLength: 100,
+        lowercase: true,
+    },
+    description: String,
+    email: {
+        type: String,
+        validate: [validator.isEmail, "Please provide a valid email."]
+    },
+    website: {
+        type: String,
+        validate: [validator.isURL, "Please provide a valid website name."]
+    },
+    location: String,
+    products: [{
+        type: ObjectId,
+        ref: "Product"
+    }],
+    suppliers: [{
+        name: String,
+        contactNumber: String,
+        id: {
+            type: ObjectId,
+            ref: "Supplier"
+        }
+    }],
+    status: {
+        type: String,
+        enum: ["active", "inactive"],
+        default: "active"
+    },
+}, { timestamps: true })
+
+const Brand = mongoose.model("Brand", brandSchema)
+
+module.export = Brand;
--- module-6/inventory-management-acc/services/product.service.js
+++ module-6/inventory-management-acc/services/product.service.js
@@ -1,4 +1,5 @@
-const Product = require("../models/product.model");
+const Product = require("../models/Product");
+
 
 // get all products 
 module.exports.getProductService = async (filters, queries) => {
